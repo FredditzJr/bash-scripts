a5198c8319b296be14cd6fe6ac80f90551bebf71
FredditzJr <samuel@debonix>
2018-06-14 10:00:02 +0200

    1.website checker in script 2.renomer new menu en menu

--- Menu.sh
+++ Menu.sh
@@ -1,20 +1,44 @@
 #!/bin/bash
 
-echo $0 
-echo $1
-echo $2
-echo $3
+chemin=$(pwd)
+green="\033[32;40m"
+red="\033[31;40m"
+none="\033[0m"
 
-exit 0
+function menu()
+{
+    OIFS=$IFS; IFS=$'\n';scripts=($(ls "scripts/"));IFS=$OIFS;
+    scripts+=("Quit")
+    COLUMNS=12
+    echo "Hello $USER"
+    sleep 1
+    echo "which script do you want to run ? : "
 
-OIFS=$IFS; IFS=$'\n';scripts=($(ls "scripts/"));IFS=$OIFS;
-COLUMNS=12
-scripts+=("Quit")
-echo "veuillez choisir un script à executer ci : "
-select script in "${scripts[@]}"; do
-    case $script in
-         "Quit") echo "Bye"; exit 0;;
-	 *) bash scripts/$script/run.sh ;;
-     esac
-done
+    select script in "${scripts[@]}"; do
+        case $script in
+            "Quit") echo "Bye"; exit 0;;
+	    *) bash scripts/$script/run.sh ;;
+        esac
+    done
+}
 
+function help()
+{
+    echo -e "Usage : bash $0 $green--[options]$none"
+    echo
+    echo -e  $green"--script [script name]"$none" 		: If you want to run a specific script."
+    echo -e  $green"--help"$none"               		: display this help."
+    echo
+    echo -e $green"If you don't pass any argument, the script will be run in interactif mode"$none
+    
+}
+
+if [ $# -eq 0 ]; then
+    menu
+else
+    case "$1" in
+        --script) bash $chemin/scripts/$2/run.sh;;
+        --help | -h) help; exit 0 ;;
+        *) echo -e $red"ERROR ! : Use --help for see option"$none; exit 1;;
+    esac
+fi
---
+++ scripts/website-checker/run.sh
@@ -0,0 +1,145 @@
+#!/bin/bash
+########################################################
+########## WEBSITE CHECKER v1.0 by FredditzJr ##########
+########################################################
+
+green="\033[32;40m"
+red="\033[31;40m"
+none="\033[0m"
+blue="\033[36;40m"
+COLUMNS=12
+chemin=$(pwd)
+
+function welcome() # Welcome  Message
+{
+    clear
+    echo -e $blue"#########################################################################################################################################################";
+    echo -e "#"$none"      _________                            .__      _________                            "$blue"#                                                             #";
+    echo -e "#"$none"     /   _____/____    _____  __ __  ____  |  |    /   _____/__ ________   ___________   "$blue"#                                                             #";
+    echo -e "#"$none"     \_____  \\__  \  /     \|  |  \_/ __ \ |  |   \_____  \|  |  \____ \_/ __ \_  __ \   "$blue"#                                                             #";
+    echo -e "#"$none"     /        \/ __ \|  Y Y  \  |  /\  ___/|  |__  /        \  |  /  |_> >  ___/|  | \/  "$blue"#                                                             #";
+    echo -e "#"$none"    /_______  (____  /__|_|  /____/  \___  >____/ /_______  /____/|   __/ \___  >__|     "$blue"#                                                             #";
+    echo -e "#"$none"            \/     \/      \/            \/               \/      |__|        \/         "$blue"#                                                             #";
+    echo -e "#"$none"     __      __      ___.     _________ .__                   __                         "$blue"#"  "Version 1.0                                                 #";
+    echo -e "#"$none"    /  \    /  \ ____\_ |__   \_   ___ \|  |__   ____   ____ |  | __ ___________         "$blue"#                                                             #";
+    echo -e "#"$none"    \   \/\/   // __ \| __ \  /    \  \/|  |  \_/ __ \_/ ___\|  |/ // __ \_  __ \        "$blue"#"  "Author :FredditzJr (samuel)                                 #";
+    echo -e "#"$none"     \        /\  ___/| \_\ \ \     \___|   Y  \  ___/\  \___|    <\  ___/|  | \/        "$blue"#                                                             #";
+    echo -e "#"$none"      \__/\  /  \___  >___  /  \______  /___|  /\___  >\___  >__|_ \\___  >__|            "$blue"#                                                             #";
+    echo -e "#"$none"           \/       \/    \/          \/     \/     \/     \/     \/    \/               "$blue"#                                                             #";
+    echo -e $blue"#########################################################################################################################################################";
+    echo -e $blue "Hello $USER" $none
+}
+
+function menu() # Choice of the option in the menu
+{
+    check=$(echo -e $blue"check sites config.txt"$none)
+    Enter=$(echo -e $blue"Enter a site"$none)
+    Quit=$(echo -e $blue"Quittez (q,Q)"$none)
+    PS3="---> select an option : "
+    OIFS=$IFS; IFS=$'\n';$things;IFS=$OIFS;
+    echo -e "$red                                    :|: MAIN MENU :|:$none"
+    echo "------------------------------------------------------------------------------------------"
+    select Choices in "$check" "$Enter" "$Quit" ;do
+        case $REPLY in
+            1) checkandsend
+               break ;;
+
+            2) Grab
+               Sending
+               send_mail
+               break ;;
+
+            3|Q*|q*) echo -e "$blue Bye ! $none"
+               break;;
+
+            *) echo -e "$blue You choice is not available ,$none  $red you must learn to read ^.^ $none";;
+         esac
+     done
+}
+
+function check() # Check the sites in the config.txt .
+{
+    liste=$(cat config.txt)
+
+    for website in $liste; do
+
+        result=$(curl -Is $website  | head -n 1 | cut -d ' ' -f 2)
+
+        if [ "$result" == "200" ]; then
+            echo -e "$green $website functionnal site ! $none"
+        else
+            echo -e "$red $website Down !!$none"
+            down+=($website)
+        fi
+
+    done
+}
+
+function Grab()  # shows and check if the site is functionnal .
+{
+    if [ $# -eq 0  ]; then
+        echo -e "$green Enter a site :example:www.google.fr $none"
+        read type
+    else
+         type=$1
+    fi
+
+    result=$(curl -Is $type  | head -n 1 | cut -d ' ' -f 2)
+
+    if [ "$result" == "200" ]; then
+         echo -e "$green functional site ! $none"
+    else
+         echo -e "$red $type Down !! $none"
+         down+=($type)
+    fi
+}
+
+function send_mail() # sending a mail for the down site ONLY !
+{
+    emails=$(cat emails.txt)
+    for liste in $emails; do
+        echo "$down :$DATE" | mail -s "Website Down !" $liste
+    done
+}
+
+function Sending() # for make wait the user
+{
+    robot=$(cat dessin.txt)
+    if [ $down="`echo $var | grep motif`" ];then
+        echo -e "$blue Sending down site , wait please ... $none"
+        tput blink; echo "$robot"; tput sgr0
+        send_mail
+    else
+        exit 0
+    fi
+}
+
+function checkandsend() # check + send function
+{
+    check
+    Sending
+}
+
+function help()   # show the main menu --help
+{
+    echo -e "Usage : bash $0 $green--[options]$none"
+    echo
+    echo -e  $green" --all "$none"              : If you want to check de site in config.txt ."
+    echo -e  $green" --site "$none"                            :Enter a site "
+    echo -e  $green" --help" $none"                               : Show this help."
+    echo
+    echo -e  $green "If you dont pass any argument, the script will be run in interactif mode" $none
+}
+
+if [ $# -eq 0 ]; then
+    welcome
+    menu
+else #option for running more fast and easy
+    case "$1" in
+        --all) checkandsend ;;
+        --site) Grap $2 ; Sending  ;;
+        --help | -h) help ; exit 0 ;;
+        *) echo -e $red"ERROR ! : Use --help for see option"$none ; exit 1;;
+    esac
+fi
+echo -e "$blue ############################################################ $none $red Bye ! $none $blue ############################################################ $none" 
